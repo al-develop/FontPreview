c1a3653cae7a85068176e7123cc1af23f0de7fe8
Dev <Dev@AL-DEV>
2022-12-01 22:03:46 +0100

    Projektdateien hinzufügen.

---
+++ WpfFontPreview/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+
+namespace WpfFontPreview
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+		List<FontFamily> _installedFonts;
+		private ObservableCollection<string> _fonts;
+		private string _selectedFont;
+		
+		public string SelectedFont
+		{
+			get { return _selectedFont; }
+			set 
+			{ 
+				SetProperty(ref _selectedFont, value, () => SelectedFont);
+			}
+		}
+		public ObservableCollection<string> Fonts
+		{
+			get { return _fonts; }
+			set { SetProperty(ref _fonts, value, () => Fonts); }
+		}
+
+
+
+		public MainWindowViewModel()
+		{
+			this.Fonts = new ObservableCollection<string>();
+			_installedFonts = new List<FontFamily>();
+
+            using (var col = new InstalledFontCollection())
+			{
+				_installedFonts = col.Families.ToList();
+				foreach (var font in _installedFonts)
+				{
+					this.Fonts.Add(font.Name);
+				}
+			}
+		}
+	}
+}
